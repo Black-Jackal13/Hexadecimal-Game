132d17c2ab3cdbbaba2728f83a0e45151dfd4b71
Black Jackal <louw.reyneke.otto@gmail.com>
2025-03-28 13:33:24 -0400

    Basic Settings Structure

---
+++ Game/settings.py
@@ -0,0 +1,19 @@
+from gpiozero import Button, LED, LEDCharDisplay
+
+PINS = [
+    None,
+    None,
+    None,
+    None,
+    None,
+    None,
+    None,
+    None,
+]
+
+LEDS = {
+    "CORRECT": None,
+    "INCORRECT": None,
+}
+
+DISPLAY = None
