57d5c2bc3cf547f32e42ebd2d9bf3f8492f72023
Black Jackal <louw.reyneke.otto@gmail.com>
2025-04-28 13:34:31 -0400

    Refactor server connection handling logic.

--- Game/RaspberryPi/server.py
+++ Game/RaspberryPi/server.py
@@ -7,10 +7,10 @@ server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # Bind Server
 server.bind((SERVER, PORT))
 
+# Connect to Host
+server.listen()
+client, address = server.accept()
+print(f"Connection from {address} has been established!")
 
-while True:
-    server.listen()
-    client, address = server.accept()
-    print(f"Connection from {address} has been established!")
-
-    client.send(b"Connected to the server!")
+client.send(b"Connected to the server!")
+client.close()
