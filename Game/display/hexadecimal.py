c69086563158a68c58a2d3daf8a5f50160b3541e
Black Jackal <louw.reyneke.otto@gmail.com>
2025-06-04 06:43:19 -0400

    Removed length check from Hexadecimal setter so that it can display one at a time.

--- Game/display/hexadecimal.py
+++ Game/display/hexadecimal.py
@@ -18,11 +18,7 @@ class Hexadecimal:
 
     @letters.setter
     def letters(self, new_letters):
-        # Check Length
-        if len(new_letters) != 2:
-            raise ValueError("Hexadecimal sequence must be 2 characters long")
-
-        # I may have forgotten the to write the line below and spent 20 mins troubleshooting, then found that I wasn't
+        # I may have forgotten to write the line below and spent 20 mins troubleshooting, then found that I wasn't
         # actually assigning the new value to the property.
         self._letters = new_letters.upper()
 
